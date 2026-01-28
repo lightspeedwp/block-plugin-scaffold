0d70ebc3ca42ba053d6dce3e78bc8ce7fa6dd0b8
Warwick <krugazul@gmail.com>
2026-01-28 12:52:04 +0200

    feat: Ensure render functions for collection and slider blocks are defined only once

--- src/blocks/{{block_slug}}-collection/render.php
+++ src/blocks/{{block_slug}}-collection/render.php
@@ -9,9 +9,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-function {{namespace}}_render_{{cpt_slug}}_collection( $attributes, $content, $block ) {
-	// Output markup for the CPT1 collection block.
-	return '<div class="wp-block-{{namespace}}-{{block_slug}}-collection">' .
-		'<p>' . esc_html__( 'CPT1 collection block output.', '{{textdomain}}' ) . '</p>' .
-	'</div>';
+if ( ! function_exists( '{{namespace}}_render_{{cpt_slug}}_collection' ) ) {
+	function {{namespace}}_render_{{cpt_slug}}_collection( $attributes, $content, $block ) {
+		// Output markup for the CPT1 collection block.
+		return '<div class="wp-block-{{namespace}}-{{block_slug}}-collection">' .
+			'<p>' . esc_html__( 'CPT1 collection block output.', '{{textdomain}}' ) . '</p>' .
+		'</div>';
+	}
 }
