0f1dff22bf17d4d07bad5398eae7160a7923ca27
Warwick <krugazul@gmail.com>
2026-02-02 14:33:25 +0200

    feat: Update block rendering to use dynamic render functions and improve file structure

--- src/blocks/{{block_slug}}-collection/render.php
+++ src/blocks/{{block_slug}}-collection/render.php
@@ -9,8 +9,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-if ( ! function_exists( '{{namespace}}_render_{{cpt_slug}}_collection' ) ) {
-	function {{namespace}}_render_{{cpt_slug}}_collection( $attributes, $content, $block ) {
+if ( ! function_exists( '{{namespace}}_render_{{cpt_slug|snakeCase}}_collection' ) ) {
+	function {{namespace}}_render_{{cpt_slug|snakeCase}}_collection( $attributes, $content, $block ) {
 		// Output markup for the CPT1 collection block.
 		return '<div class="wp-block-{{namespace}}-{{block_slug}}-collection">' .
 			'<p>' . esc_html__( 'CPT1 collection block output.', '{{textdomain}}' ) . '</p>' .
